ba22d140ce076e1b27cbdd01bda69ce9487feedc
Syam kumar Allaparthi <syamkumar.allaparthi@gmail.com>
2022-11-09 18:39:12 -0600

    added view_all to fuel.js

--- controllers/fuel.js
+++ controllers/fuel.js
@@ -12,7 +12,16 @@ exports.fuel_list = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 };
- 
+exports.fuel_view_all_Page = async function(req, res) { 
+    try{ 
+        theFuels = await Fuel.find(); 
+        res.render('fuel', { title: 'Fuel Search Results', results: theFuels }); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+};
 // for a specific Fuel. 
 exports.fuel_detail = function(req, res) { 
     res.send('NOT IMPLEMENTED: Fuel detail: ' + req.params.id); 
--- routes/fuel.js
+++ routes/fuel.js
@@ -1,9 +1,10 @@
 var express = require('express');
+const fuel_controlers= require('../controllers/fuel')
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('fuel', { title: 'Search Results Fuel' });
-});
-
+// router.get('/', function(req, res, next) {
+//   res.render('fuel', { title: 'Search Results Fuel' });
+// });
+router.get('/', fuel_controlers.fuel_view_all_Page );
 module.exports = router;
