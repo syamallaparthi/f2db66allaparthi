618d3c26ed130150de5c21bc28f77cb94082bb78
Syam kumar Allaparthi <syamkumar.allaparthi@gmail.com>
2022-11-10 20:59:26 -0600

    updated as per the lab

--- controllers/fuel.js
+++ controllers/fuel.js
@@ -59,6 +59,21 @@ exports.fuel_delete = function(req, res) {
 }; 
  
 // Handle Fuel update form on PUT. 
-exports.fuel_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Fuel update PUT' + req.params.id); 
-};
+exports.fuel_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+        ${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Fuel.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.Category) toUpdate.Category = req.body.Category; 
+        if(req.body.Color) toUpdate.color = req.body.color; 
+        if(req.body.Quantity) toUpdate.Quantity = req.body.Quantity; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
